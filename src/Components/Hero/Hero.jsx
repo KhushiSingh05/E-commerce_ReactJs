8c9c2659589a784709640264c845d8e654b3bde0
KhushiSingh05 <ksingh16851@gmail.com>
2025-08-16 14:03:23 +0530

    few updates

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import hero_img from '../../Assets/f1.jpg'
 import './Hero.css'
+import { useNavigate } from "react-router-dom";
 
 const Hero = () => {
+   const navigate = useNavigate();
+  const handleCheckout = () => {
+    navigate("/Products");
+  }
   return (
     <div>
       <div className="hero">
         <div className="hero_left">
           <h2>Unleash Premium Clothing Quality </h2>
           <p>whether you want to shop for corporate wears,casual wears, men's wear or kids wear, We have comfortable clothes to elevate everyday look</p>
-          <button>Shop Now</button>
+          <button onClick={handleCheckout}>Shop Now</button>
         </div>
         <div className="hero_right">
           <img src={hero_img} alt="hero" />
